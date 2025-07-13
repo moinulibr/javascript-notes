6dac71c80cb94837353021b2b44fd972318c924b
Md. Moinul Islam <moinulibr@gmail.com>
2025-07-13 23:11:45 +0600

    root index file modify

---
+++ modules/project/src/index.js
@@ -0,0 +1,8 @@
+import { Button } from './components/Button/Button.js'; // আপেক্ষিক পাথ
+
+const myButton = Button({
+    text: "Click Me!",
+    onClick: () => alert("Button clicked!")
+});
+
+document.body.appendChild(myButton);
